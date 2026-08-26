66071a293416571599cf7687051d1f41041e8ba0
Mirza Zubier Siddique <128462721+MirzaRafi900@users.noreply.github.com>
2026-08-27 00:17:40 +0800

    Practice

--- 03-news-article-analyzer/main.py
+++ 03-news-article-analyzer/main.py
@@ -1,43 +1,42 @@
 import spacy
 from collections import Counter
 
-# Load Model
+# Import Library
 nlp = spacy.load("en_core_web_sm")
 
-# User Input
-article = input("📰 Paste a news article here: \n\n")
+# Input Article
+article = input("\n📰 Paste the Article Here:\n\n")
 
 # Process Text
 doc = nlp(article)
 
-print("\n" + "_" * 50)
+print("\n" + "=" * 50)
 print("📰 News Article Analyzer")
-print("_" * 30)
+print("_" * 50)
 
-# Name Entities
+# Named Entities
 print("\n📌 Named Entities")
 print("_" * 30)
 
 for ent in doc.ents:
     print(f"{ent.text:<25} {ent.label_}")
 
-# Important Words
-words = [
+# Important Word
+word=[
     token.text.lower()
     for token in doc
     if token.is_alpha and not token.is_stop
 ]
 
-counter = Counter(words)
+counter = Counter(word)
 
-print("\n🔥 TOP KEYWORDS")
+print("\n🔥 Top Keywords")
 print("_" * 30)
 
 for word, count in counter.most_common(5):
-    print(f"{word:<25} {count}")
+    print(f"{word:<15} {count}")
 
 # POS Tag Count
-
 pos_tags = Counter(token.pos_ for token in doc)
 
 print("\n📊 Parts of Speech")
@@ -45,8 +44,6 @@ print("_" * 30)
 
 for pos, count in pos_tags.items():
     print(f"{pos:<10} {count}")
-
-
 
 
 
